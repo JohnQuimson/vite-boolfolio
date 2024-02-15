5137e3f03c908fbcc6c2c75a6a09523da9f09e8d
JohnQuimson <john.quimson777@gmail.com>
2024-02-15 17:02:16 +0100

    feat: chiamata axios

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script>
+import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
-import { store } from './store.js';
+// import { store } from './store.js';
 
 export default {
   name: 'App',
+
+  data() {
+    return {
+      baseUrl: 'http://127.0.0.1:8000',
+      apiUrls: {
+        projects: '/api/projects',
+      },
+    };
+  },
 
   components: {
     AppHeader,
@@ -13,10 +23,20 @@ export default {
     AppFooter,
   },
 
-  data() {
-    return {
-      store,
-    };
+  methods: {
+    getProjects() {
+      axios
+        .get(this.baseUrl + this.apiUrls.projects)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.getProjects();
   },
 };
 </script>
