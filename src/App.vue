373251fd64c54a7e3634bdaa4afe41183beb91c9
JohnQuimson <john.quimson777@gmail.com>
2024-02-18 19:28:38 +0100

    feat: refactor main vue

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,14 @@
 <script>
-import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
 // import { store } from './store.js';
 
 export default {
   name: 'App',
 
-  data() {
-    return {
-      projects: [],
-      baseUrl: 'http://127.0.0.1:8000',
-      apiUrls: {
-        projects: '/api/projects',
-      },
-    };
-  },
-
   components: {
     AppHeader,
-    AppMain,
     AppFooter,
-  },
-
-  methods: {
-    getProjects() {
-      axios
-        .get(this.baseUrl + this.apiUrls.projects)
-        .then((response) => {
-          this.projects = response.data.data;
-          console.log(response);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-  },
-  created() {
-    this.getProjects();
   },
 };
 </script>
--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -1,9 +1,48 @@
 <script>
+import axios from 'axios';
+import ProjectCard from '../components/ProjectCard.vue';
+
 export default {
   name: 'AppList',
+
+  data() {
+    return {
+      projects: [],
+      baseUrl: 'http://127.0.0.1:8000',
+      apiUrls: {
+        projects: '/api/projects',
+      },
+    };
+  },
+
+  components: {
+    ProjectCard,
+  },
+
+  methods: {
+    getProjects() {
+      axios
+        .get(this.baseUrl + this.apiUrls.projects)
+        .then((response) => {
+          this.projects = response.data.data;
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.getProjects();
+  },
 };
 </script>
 
 <template>
-  <div>List Page</div>
+  <main>
+    <h1 class="text-center mt-5">Projects</h1>
+    <div class="container py-5 d-flex flex-wrap justify-content-between gap-5">
+      <ProjectCard :projects="projects" />
+    </div>
+  </main>
 </template>
