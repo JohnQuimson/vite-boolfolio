e47b3842821b7bd406adbcaaeaf5e0e3e92f55ed
JohnQuimson <john.quimson777@gmail.com>
2024-02-29 23:34:52 +0100

    feat: about img

--- src/pages/AppAbout.vue
+++ src/pages/AppAbout.vue
@@ -7,8 +7,14 @@ export default {
 <template>
   <main>
     <div class="container">
-      <div class="cont-img">foto</div>
-      <div class="info">info</div>
+      <div class="left">
+        <div class="cont-img">
+          <img src="../../public/img/me.png" alt="" />
+        </div>
+      </div>
+      <div class="right">
+        <div class="cont-info">info</div>
+      </div>
     </div>
   </main>
 </template>
@@ -22,15 +28,30 @@ main {
   color: white;
   .container {
     display: flex;
+    margin: 0 auto;
+    padding-top: 100px;
 
-    .cont-img {
+    .left {
       width: 40%;
       border: 1px solid red;
+
+      .cont-img {
+        width: 60%;
+        margin: 0 auto;
+
+        img {
+          width: 100%;
+          height: 100%;
+        }
+      }
     }
 
-    .info {
+    .right {
       width: 60%;
       border: 1px solid yellow;
+
+      .cont-info {
+      }
     }
   }
 }
